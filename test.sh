e1d2862ee78b52e282328fd27bd29538e0103e34
yoyotv <extra71376@gmail.com>
2020-05-08 11:58:46 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ echo "$output"
 
 
 curl -X POST --data-urlencode "payload={\"message\": \"yuhung\", \"username\": \"CONVID-19\", \"text\": \"$output in Taiwan\"}" https://hooks.slack.com/services/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
-
+# send a message to slack 
 
 
